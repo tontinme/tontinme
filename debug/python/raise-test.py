98aca12777e6b0eff976629071ef0917ea1aa3bb
cinder <woenjoy@gmail.com>
2011-07-24 00:17:25 +0800

    del debug

--- debug/python/raise-test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-def inputAge():
-	age = input("Input your age:")
-	if (age>100 or age<18):
-		raise 'BadAgeError', 'out of range'
-	return age
-if __name__ == '__main__':
-	inputAge()
--- debug/shell/global-local_var.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-VAR="global variable"
-function bash {
-	local VAR="local variable"
-	echo $VAR
-}
-echo $VAR
-bash
-echo $VAR
